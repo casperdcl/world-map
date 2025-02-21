fba47f9f881215543c0c66d6d20c36304148fcca
Casper da Costa-Luis <casper.dcl@physics.org>
2025-02-21 12:30:43 +0000

    re-enable four-colour

--- world_map.py
+++ world_map.py
@@ -9,13 +9,12 @@ import networkx as nx
 import numpy as np
 import svgwrite
 import svgwrite.container
-from matplotlib import colormaps
 from PIL import Image
 from shapely import box
 
 __author__ = "Casper da Costa-Luis (https://github.com/casperdcl)"
 __license__ = "MPL-2.0"
-__version__ = "0.0.0"
+__version__ = "0.1.0"
 
 # Eckert II, ESRI:53014, https://epsg.io/53014
 to_crs = "+proj=eck2 +lon_0=0 +x_0=0 +y_0=0 +R=6371000 +units=m +no_defs +type=crs"
@@ -56,10 +55,10 @@ if not (fname := Path("ne_50m_relief.jpg")).is_file():
 
     Image.fromarray(arr.transpose(1, 2, 0)).save(fname, quality=100)
 
-# %% Build graph of countries & colour them
-
+# %% Build graph of countries & use four-colour theorem
 water = '#6baed6'
-# colour based on four-colour theorem
+red, blue, green, yellow, black = 'red', 'blue', 'green', 'yellow', 'black'
+four_colour = True
 
 G = nx.Graph()
 for iii, row in gdf.iterrows():
@@ -69,15 +68,16 @@ for iii, row in gdf.iterrows():
 colors = nx.coloring.greedy_color(G, strategy="smallest_last")
 
 
-def rgb2hex(rgb):
-    return '#%02x%02x%02x' % tuple(int(v * 255) for v in rgb)
+if four_colour:
+    from matplotlib import colormaps
 
+    def rgb2hex(rgb):
+        return '#%02x%02x%02x' % tuple(int(v * 255) for v in rgb)
 
-color_map = list(map(rgb2hex, np.asarray(colormaps.get('Set2').colors)[[4, 5, 6, 3, 1]]))
+    color_map = list(map(rgb2hex, np.asarray(colormaps.get('Set2').colors)[[4, 5, 6, 3, 1]]))
+    # red, blue, green, _, _, yellow, _, _, _ = map(rgb2hex, colormaps.get('Pastel1').colors)
+    # black = rgb2hex(colormaps.get('Pastel2').colors[-1])
 
-# red, blue, green, _, _, yellow, _, _, _ = map(rgb2hex, colormaps.get('Pastel1').colors)
-# black = rgb2hex(colormaps.get('Pastel2').colors[-1])
-red, blue, green, yellow, black = 'red', 'blue', 'green', 'yellow', 'black'
 olympic_colors = {
     'Africa': green,
     'Antarctica': 'white',
@@ -102,11 +102,12 @@ g = svgwrite.container.Group(id='relief')
 dwg.add(dwg.image(href=f"data:image/jpeg;base64,{b64encode(Path(fname).read_bytes()).decode()}",
                   height='100%', width='100%', x=viewbox[0], y=viewbox[1]))
 
-g = svgwrite.container.Group(id='countries', opacity=0.15)
+g = svgwrite.container.Group(id='countries', opacity=0.25)
 for continent, fill in olympic_colors.items():
     gcont = svgwrite.container.Group(id=continent.replace(' ', '_').replace('(', '').replace(')', ''), fill=fill)
     for row in gdf[gdf['CONTINENT'] == continent].itertuples():
-        # fill = 'white' if row['NAME'] == "Antarctica" else rgba_to_hex(color_map[colors[i]])
+        if four_colour:
+            fill = 'white' if continent == "Antarctica" else color_map[colors[row.Index]]
         gc = svgwrite.container.Group(id=row.NAME.replace(' ', '_'))
         for poly in getattr(row.geometry, 'geoms', [row.geometry]):
             gc.add(dwg.polygon(points=np.asanyarray(poly.exterior.coords.xy).T[:-1] * [scale, -scale], fill=fill))
